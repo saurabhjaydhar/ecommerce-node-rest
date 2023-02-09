fe73f727b3c5184f7d1a03c132fbdb244479c463
Saurabh Jaydhar <64015173+saurabhjaydhar@users.noreply.github.com>
2023-02-09 16:34:50 +0530

    chnage

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ const connectDB = () => {
   mongoose
     .connect(
       'mongodb+srv://saurabh:qwerty1234@cluster0.1cm0jr7.mongodb.net/auth?retryWrites=true&w=majority',
+      { keepAlive: true, keepAliveInitialDelay: 300000 },
     )
     .then((value) => {
       console.log(`mongoose connection done | ${value}`);
@@ -12,7 +13,7 @@ const connectDB = () => {
     .catch((error) => {
       console.log(`mongoose connection failed | ${error}`);
     });
-  mongoose.set('strictQuery', true);
+  // mongoose.set('strictQuery', true);
 };
 
 module.exports = connectDB;
