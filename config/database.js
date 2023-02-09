571854471cfcf7f355235442ac98eaf77baaefbe
Saurabh Jaydhar <64015173+saurabhjaydhar@users.noreply.github.com>
2023-02-09 15:36:36 +0530

    chnage to string

--- config/database.js
+++ config/database.js
@@ -4,7 +4,6 @@ const dotenv = require('dotenv');
 const connectDB = () => {
   mongoose
     .connect(
-      // process.env.DB_URI
       'mongodb+srv://saurabh:qwerty1234@cluster0.1cm0jr7.mongodb.net/?retryWrites=true&w=majority',
       { useNewUrlParser: true },
     )
