6ffbd35165386c10d1151c32d52ceac779b2a19c
Saurabh Jaydhar <64015173+saurabhjaydhar@users.noreply.github.com>
2023-02-09 15:24:28 +0530

    chnage path

--- config/database.js
+++ config/database.js
@@ -3,7 +3,10 @@ const dotenv = require('dotenv');
 
 const connectDB = () => {
   mongoose
-    .connect(process.env.DB_URI)
+    .connect(
+      // process.env.DB_URI
+      'mongodb+srv://saurabh:qwerty1234@cluster0.1cm0jr7.mongodb.net/?retryWrites=true&w=majority',
+    )
     .then((value) => {
       console.log(`mongoose connection done | ${value}`);
     })
